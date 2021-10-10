57f0cbdf2415aeff79ba1a8f5d714593dcf803ce
Valerii Vitenko <valero2313@gmail.com>
2021-10-10 18:26:36 +0300

    moved to useState and useEffect hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { toast, ToastContainer, Zoom } from 'react-toastify';
 import Loader from 'react-loader-spinner';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,36 +10,23 @@ import ImageGallery from '../ImageGallery/ImageGallery';
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
 
-export default class App extends Component {
-  state = {
-    text: '',
-    page: 1,
-    images: [],
-    showModal: false,
-    error: null,
-    status: 'idle',
-    largeImageURL: null,
-    tags: null,
-  };
+export default function App() {
+  const [text, setText] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [largeImageURL, setLargeImageURL] = useState(null);
+  const [tags, setTags] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.text;
-    const nextQuery = this.state.text;
-
-    if (prevQuery !== nextQuery) {
-      this.setState({ images: [], page: 1 });
-      this.fetchImages();
-    }
-  }
-
-  fetchImages = () => {
+  const fetchImages = () => {
     const API_KEY = '22952317-a5881606497ad665bd114491c';
     const BASE_URL = 'https://pixabay.com/api';
-    const { text, page } = this.state;
     const perPage = 12;
     const request = `/?image_type=photo&orientation=horizontal&q=${text}&page=${page}&per_page=${perPage}&key=${API_KEY}`;
 
-    this.setState({ status: 'pending' });
+    setStatus('pending');
 
     fetch(BASE_URL + request)
       .then(res => res.json())
@@ -47,71 +35,186 @@ export default class App extends Component {
         if (images.length < 1) {
           toast.error('Nothing found, specify your search');
         }
-        this.setState(prevState => ({
-          images: [...prevState.images, ...images],
-          status: 'resolved',
-          page: prevState.page + 1,
-        }));
+        setImages(prev => [...prev, ...images]);
+        setStatus('resolved');
+        setPage(prev => prev + 1);
         if (page !== 1) {
-          this.handlePageScroll();
+          handlePageScroll();
         }
       })
-      .catch(error => this.setState({ error, status: 'rejected' }));
+      .catch(error => {
+        setError(error);
+        console.error(error.message);
+        setStatus('rejected');
+      });
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  useEffect(() => {
+    if (!text) return;
+    fetchImages();
+  }, [text]);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  setModalImgInfo = ({ largeImageURL, tags }) => {
-    this.setState({ largeImageURL, tags });
-    this.toggleModal();
+  const setModalImgInfo = ({ largeImageURL, tags }) => {
+    setLargeImageURL(largeImageURL);
+    setTags(tags);
+    toggleModal();
   };
 
-  handlePageScroll = () => {
+  const handlePageScroll = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
   };
 
-  onSubmit = text => {
-    this.setState({ text, page: 1 });
+  const onSubmit = query => {
+    if (query === text) return;
+    setText(query);
+    setPage(1);
+    setImages([]);
   };
 
-  onLoadMore = () => {
-    this.fetchImages();
+  const onLoadMore = () => {
+    fetchImages();
   };
 
-  render() {
-    const { images, showModal, largeImageURL, tags, status } = this.state;
-    const showLoadMore = images.length > 0 && images.length >= 12;
-    return (
-      <>
-        <div className="App">
-          <Searchbar onSubmit={this.onSubmit} />
-          {status === 'pending' && (
-            <Loader
-              className="Loader"
-              type="Puff"
-              color="#00BFFF"
-              height={150}
-              width={150}
-              timeout={2000}
-            />
-          )}
-          <ImageGallery images={images} setModalImgInfo={this.setModalImgInfo} />
-          {showLoadMore && <Button onLoadMore={this.onLoadMore} />}
-          {showModal && (
-            <Modal onClose={this.toggleModal}>
-              <img src={largeImageURL} alt={tags} />
-            </Modal>
-          )}
-        </div>
-        <ToastContainer transition={Zoom} autoClose={3000} />
-      </>
-    );
-  }
+  const showLoadMore = images.length > 0 && images.length >= 12;
+
+  return (
+    <>
+      <div className="App">
+        <Searchbar onSubmit={onSubmit} />
+        {status === 'pending' && (
+          <Loader
+            className="Loader"
+            type="Puff"
+            color="#00BFFF"
+            height={150}
+            width={150}
+            timeout={2000}
+          />
+        )}
+        <ImageGallery images={images} setModalImgInfo={setModalImgInfo} />
+        {showLoadMore && <Button onLoadMore={onLoadMore} />}
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <img src={largeImageURL} alt={tags} />
+          </Modal>
+        )}
+      </div>
+      <ToastContainer transition={Zoom} autoClose={3000} />
+    </>
+  );
 }
+
+// * WORKING CLASS COMPONENT
+// export default class App extends Component {
+//   state = {
+//     text: '',
+//     page: 1,
+//     images: [],
+//     showModal: false,
+//     error: null,
+//     status: 'idle',
+//     largeImageURL: null,
+//     tags: null,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevQuery = prevState.text;
+//     const nextQuery = this.state.text;
+
+//     if (prevQuery !== nextQuery) {
+//       this.setState({ images: [], page: 1 });
+//       this.fetchImages();
+//     }
+//   }
+
+// fetchImages = () => {
+//   const API_KEY = '22952317-a5881606497ad665bd114491c';
+//   const BASE_URL = 'https://pixabay.com/api';
+//   const { text, page } = this.state;
+//   const perPage = 12;
+//   const request = `/?image_type=photo&orientation=horizontal&q=${text}&page=${page}&per_page=${perPage}&key=${API_KEY}`;
+
+//   this.setState({ status: 'pending' });
+
+//   fetch(BASE_URL + request)
+//     .then(res => res.json())
+//     .then(array => {
+//       const images = array.hits;
+//       if (images.length < 1) {
+//         toast.error('Nothing found, specify your search');
+//       }
+// this.setState(prevState => ({
+//   images: [...prevState.images, ...images],
+//   status: 'resolved',
+//   page: prevState.page + 1,
+// }));
+//       if (page !== 1) {
+//         this.handlePageScroll();
+//       }
+//     })
+//     .catch(error => this.setState({ error, status: 'rejected' }));
+// };
+
+// toggleModal = () => {
+//   this.setState(({ showModal }) => ({
+//     showModal: !showModal,
+//   }));
+// };
+
+// setModalImgInfo = ({ largeImageURL, tags }) => {
+//   this.setState({ largeImageURL, tags });
+//   this.toggleModal();
+// };
+
+// handlePageScroll = () => {
+//   window.scrollTo({
+//     top: document.documentElement.scrollHeight,
+//     behavior: 'smooth',
+//   });
+// };
+
+// onSubmit = text => {
+//   this.setState({ text, page: 1 });
+// };
+
+// onLoadMore = () => {
+//   this.fetchImages();
+// };
+
+//   render() {
+//     const { images, showModal, largeImageURL, tags, status } = this.state;
+//     const showLoadMore = images.length > 0 && images.length >= 12;
+// return (
+//   <>
+//     <div className="App">
+//       <Searchbar onSubmit={this.onSubmit} />
+//       {status === 'pending' && (
+//         <Loader
+//           className="Loader"
+//           type="Puff"
+//           color="#00BFFF"
+//           height={150}
+//           width={150}
+//           timeout={2000}
+//         />
+//       )}
+//       <ImageGallery images={images} setModalImgInfo={this.setModalImgInfo} />
+//       {showLoadMore && <Button onLoadMore={this.onLoadMore} />}
+//       {showModal && (
+//         <Modal onClose={this.toggleModal}>
+//           <img src={largeImageURL} alt={tags} />
+//         </Modal>
+//       )}
+//     </div>
+//     <ToastContainer transition={Zoom} autoClose={3000} />
+//   </>
+// );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,54 +1,90 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import './Searchbar.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default class Searchbar extends Component {
-  state = {
-    text: '',
+export default function Searchbar({ onSubmit }) {
+  const [text, setText] = useState('');
+
+  const handleChange = event => {
+    setText(event.target.value);
   };
 
-  handleChange = event => {
-    this.setState({
-      text: event.target.value,
-    });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.text.trim() === '') {
-      return toast.error('Enter city name');
+    if (text.trim() === '') {
+      return toast.error('Enter valid search');
     }
-    this.props.onSubmit(this.state.text);
-    this.setState({
-      text: '',
-    });
+    onSubmit(text);
+    setText('');
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
 
-          <input
-            value={this.state.text}
-            onChange={this.handleChange}
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          value={text}
+          onChange={handleChange}
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+// export default class Searchbar extends Component {
+//   state = {
+//     text: '',
+//   };
+
+//   handleChange = event => {
+//     this.setState({
+//       text: event.target.value,
+//     });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.text.trim() === '') {
+//       return toast.error('Enter city name');
+//     }
+//     this.props.onSubmit(this.state.text);
+//     this.setState({
+//       text: '',
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <header className="Searchbar">
+//         <form className="SearchForm" onSubmit={this.handleSubmit}>
+//           <button type="submit" className="SearchForm-button">
+//             <span className="SearchForm-button-label">Search</span>
+//           </button>
+
+//           <input
+//             value={this.state.text}
+//             onChange={this.handleChange}
+//             className="SearchForm-input"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
